6d2641c32faeccd59fb2a093fcb8336c86704112
hardikmotwani <hardik.motwani@rapidops.com>
2020-04-09 16:23:04 +0530

    update file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Data from './components/Data'
+
 // import'bootstrap/dist/css/bootstrap.css';
 // import { Button } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
+      
       <Data />
     </div>
   );
